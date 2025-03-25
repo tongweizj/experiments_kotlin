4b029873453746df2a68a0a575aee33872ed3424
Max <tongweizj@gmail.com>
2025-03-24 22:41:46 -0400

    20250324 22：41 完成基本功

--- chapter07/app/src/main/java/com/packt/chapterseven/views/CitiesScreen.kt
+++ chapter07/app/src/main/java/com/packt/chapterseven/views/CitiesScreen.kt
@@ -3,6 +3,7 @@ package com.packt.chapterseven.views
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,29 +11,33 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.packt.chapterseven.data.City
+import com.packt.chapterseven.navigation.ContentType
+import com.packt.chapterseven.viewmodel.PetsViewModel
 import kotlinx.serialization.InternalSerializationApi
+import org.koin.androidx.compose.koinViewModel
 
 @OptIn(InternalSerializationApi::class)
 @Composable
-fun CityList(
+fun CitiesScreen(
     onPetClicked: (City) -> Unit,
-    modifier: Modifier = Modifier,
-    cityList:List<City>
+    contentType: ContentType,
 ) {
-     LazyColumn(modifier = Modifier.padding(16.dp)) {
-            items(cityList) { city ->
-                cityListItem(
-                    city,
-                    onPetClicked = onPetClicked
-                        )
-            }
-            // Add more content here
-        }
-    }
+    val petsViewModel: PetsViewModel = koinViewModel()
+    val petsUIState by petsViewModel.petsUIState.collectAsStateWithLifecycle()
+    CityScreenContent(
+        modifier = Modifier
+            .fillMaxSize(),
+        onCityClicked = onPetClicked,
+        contentType = contentType,
+        petsUIState = petsUIState
+    )
+}
 
