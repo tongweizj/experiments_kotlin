640e2f3485b45867e1698ba6a917366fffa293bd
Max <tongweizj@gmail.com>
2025-04-14 00:10:46 -0400

    update

--- WanderLensOntario/app/src/main/java/com/packt/chapterseven/views/MapContent.kt
+++ WanderLensOntario/app/src/main/java/com/packt/chapterseven/views/MapContent.kt
@@ -1,179 +1,48 @@
 package com.packt.chapterseven.views
 
 import android.widget.Toast
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.PolyUtil
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.google.maps.android.compose.Marker
-import com.packt.chapterseven.data.City
-import com.packt.chapterseven.viewmodel.CitiesViewModel
-import kotlinx.serialization.InternalSerializationApi
-import org.koin.androidx.compose.koinViewModel
 import com.google.maps.android.compose.rememberMarkerState
-import com.google.maps.android.PolyUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.style.TextOverflow
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.CameraUpdateFactory
-import retrofit2.Response
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-@OptIn(ExperimentalMaterial3Api::class, InternalSerializationApi::class)
-@Composable
-fun WanderlensDetailsScreen(
-    onBackPressed: () -> Unit,
-    cityID: String
-) {
-    val citiesViewModel: CitiesViewModel = koinViewModel()
-    val citiesUIState by citiesViewModel.citiesUIState.collectAsStateWithLifecycle()
-    val city = citiesUIState.cityList[cityID.toInt()]
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Color.White,
-                    titleContentColor = Color.Black, // 标题文字颜色
-                    navigationIconContentColor = Color.Black // 返回图标颜色
-                ),
-                title = {
-                    Text(
-                        city.name,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = onBackPressed,
-                        content = {
-                            Icon(
-                                imageVector = Icons.Default.ArrowBack,
-                                contentDescription = "Back",
-                                tint = Color.Black
-                            )
-                        }
-                    )
-                },
-                actions={Icon(
-                        modifier = Modifier
-                            .clickable {
-                                    citiesViewModel.updatePet(city.copy(isFavorite = !city.isFavorite))
-                            },
-                imageVector = if (city.isFavorite) {
-                    Icons.Default.Favorite
-                } else {
-                    Icons.Default.FavoriteBorder
-                },
-                contentDescription = "Favorite",
-                tint = if (city.isFavorite) {
-                    Color.Red
-                } else {
-                    Color.Gray
-                },
-            )},
-
-                    scrollBehavior = scrollBehavior,
-            )
-        },
-        content = { paddingValues ->
-            PetDetailsScreenContent(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(paddingValues),
-                city = city,
-                onFavoriteClicked = {
-                    citiesViewModel.updatePet(it)
-                }
-            )
-        }
-    )
-}
-
-@OptIn(ExperimentalLayoutApi::class, InternalSerializationApi::class)
-@Composable
-fun PetDetailsScreenContent(
-    modifier: Modifier,
-    city: City,
-
-    onFavoriteClicked: (City) -> Unit
-) {
-    Column(
-        modifier = modifier.fillMaxHeight(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .padding(1.dp)
-        ) {
-            // 根据城市设置不同的坐标点
-            val destinationLocation = LatLng(city.latitude, city.longitude)
-
-            // 假设用户当前位置（实际应用中应该获取真实位置）
-            val sourceLocation = LatLng(43.8673, -79.3358)
-
-            MapScreen(
-                sourceLocation = sourceLocation,
-                destinationLocation = destinationLocation
-            )
-        }
-    }
-}
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun MapScreen(
+fun MapContent(
     sourceLocation: LatLng,
     destinationLocation: LatLng
 ) {
@@ -275,16 +144,14 @@ fun MapScreen(
         )
     ) {
         // 标记和路径
-        currentLocation.let {
-            Marker(
-                state = rememberMarkerState(position = it),
-                title = "起点",
-                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
-            )
-        }
+        Marker(
+            state = rememberMarkerState(position = currentLocation),
+            title = "Current Location",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
         Marker(
             state = rememberMarkerState(position = destinationLocation),
-            title = "终点",
+            title = "Destination Location",
             icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
         )
         polylinePoints?.let { points ->
@@ -294,6 +161,8 @@ fun MapScreen(
                 width = 12f
             )
         }
+
+
     }
 
     // 处理权限被拒绝的情况
@@ -310,6 +179,7 @@ fun MapScreen(
         )
     }
 }
+
 
 // Directions API服务接口
 interface DirectionsService {
